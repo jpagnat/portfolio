06b11e8ab85014d901b66eed9db45a0a4ea86c52
jpagnat <pagnat.jm@gmail.com>
2022-10-16 21:39:42 +0200

    Flip cards projects

--- js/work.js
+++ js/work.js
@@ -9,7 +9,7 @@ const proyectos = [
     tecnologias: "HTML, CSS, JavaScript, JSON",
   },
   {
-    title: "Credit Card Interactive",
+    title: "Card Interactive",
     img: "./img/proyectos/proyecto3.png",
     description: "Proyecto Frontend Mentor",
     back: "Sitio web responsive con formulario dinamico y tarjeta actualizada en tiempo real",
@@ -41,7 +41,21 @@ const cardContainer = document.querySelector(".work__projects");
 
 function loadCards() {
   for (let i = 0; i < proyectos.length; i++) {
-    cardContainer.innerHTML += `<div class="work__element" id=${i}></div>`;
+    cardContainer.innerHTML += `
+    <div class="card">
+      <div class="content">
+        <div id=${i} class="front"></div>
+        <div class="back">
+          <h3 class='back__title'>${proyectos[i].title}</h3>
+          <p class='back__description'>${proyectos[i].description}</p>
+          <p class='back__tecnologias'>< ${proyectos[i].tecnologias} ></p>
+          <div class='back__a'>
+            <a href=${proyectos[i].github} target='_blank'><i class="fa-solid fa-code"></i></a>
+            <a href=${proyectos[i].page} target='_blank'>Demo <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>`;
 
     const el = document.getElementById(i);
     el.style.backgroundImage = `url(${proyectos[i].img})`;
